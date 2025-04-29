df03f46099dd16c883816acde6aaf3058c5507fb
Isabella <isabellatm1@al.insper.edu.br>
2025-04-29 12:14:30 -0300

    Guardar dados

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,10 @@ def rolar_dados (numero):
         dados.append(dado)
         i += 1
     return dados
+
+def guardar_dado (dados_rolados, dados_guardados, dado_armazenado):
+    tirar = dados_rolados[dado_armazenado]
+    dados_guardados.append(tirar)
+    del dados_rolados[dado_armazenado]
+    dados_no_estoque = [dados_rolados, dados_guardados]
+    return dados_no_estoque
