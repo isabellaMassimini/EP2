e5cb36eb45c436bec609adc96ff565b590dda080
Isabella <isabellatm1@al.insper.edu.br>
2025-04-30 08:49:44 -0300

    funcao remover dado até calcula pontos sequencia alta

--- funcoes.py
+++ funcoes.py
@@ -14,3 +14,79 @@ def guardar_dado (dados_rolados, dados_guardados, dado_armazenado):
     del dados_rolados[dado_armazenado]
     dados_no_estoque = [dados_rolados, dados_guardados]
     return dados_no_estoque
+
+def remover_dado(dados_rolados, dados_guardados, dado_para_remover):
+    remover = dados_guardados[dado_para_remover]
+    dados_rolados.append(remover)
+    del dados_guardados[dado_para_remover]
+    return [dados_rolados, dados_guardados]
+
+def calcula_pontos_regra_simples (face_dados):
+    dic = {}
+    i = 0
+    soma1 = 0
+    soma2 = 0
+    soma3 = 0
+    soma4 = 0
+    soma5 = 0
+    soma6 = 0 
+    while i < len(face_dados):
+        if face_dados[i] == 1:
+            soma1 += 1
+        if face_dados[i] == 2:
+            soma2 += 2
+        if face_dados[i] == 3:
+            soma3 += 3
+        if face_dados[i] == 4:
+            soma4 += 4
+        if face_dados[i] == 5:
+            soma5 += 5
+        if face_dados[i] == 6:
+            soma6 += 6
+        i += 1
+    dic[1] = soma1
+    dic[2] = soma2
+    dic[3] = soma3
+    dic[4] = soma4
+    dic[5] = soma5
+    dic[6] = soma6
+    return dic 
+
+def calcula_pontos_soma (valores):
+    i = 0
+    somat = 0
+    while i < len(valores):
+        somat += valores[i]
+        i+= 1
+    return somat 
+
+def calcula_pontos_sequencia_baixa(face_dado):
+    presenca = [0, 0, 0, 0, 0, 0]
+
+    i = 0
+    while i < len(face_dado):
+        valor = face_dado[i]
+        if 1 <= valor <= 6:
+            presenca[valor - 1] = 1
+        i += 1
+
+    j = 0
+    while j <= 2: 
+        if presenca[j] == 1 and presenca[j+1] == 1 and presenca[j+2] == 1 and presenca[j+3] == 1:
+            return 15
+        j += 1
+
+    return 0
+
+def calcula_pontos_sequencia_alta(face_dado):
+    presenca = [0, 0, 0, 0, 0, 0]
+
+    for valor in face_dado:
+        if 1 <= valor <= 6:
+            presenca[valor - 1] = 1
+
+    for i in range(2):  
+        if presenca[i] and presenca[i+1] and presenca[i+2] and presenca[i+3] and presenca[i+4]:
+            return 30
+
+    return 0 
